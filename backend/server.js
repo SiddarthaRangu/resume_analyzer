45c5b06e656e30c9738e3959d87dbd3916cce46f
SiddarthaRangu <siddartharangu@gmail.com>
2025-09-07 21:42:27 +0530

    added frontend link in allowed

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5001;
 
 const allowedOrigins = [
   'http://localhost:3000', 
-  'YOUR_NETLIFY_FRONTEND_URL' 
+  'https://resume-analyzer-pied.vercel.app/' 
 ];
 
 app.use(cors({
